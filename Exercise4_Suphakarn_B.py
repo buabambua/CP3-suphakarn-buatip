cdfd777b054d837f0cedd22cc400f919370bcd30
buabambua <subhakarn.bua@gmail.com>
2025-06-30 15:59:29 +0200

    Update Exercise4_Suphakarn_B.py

--- Exercise4_Suphakarn_B.py
+++ Exercise4_Suphakarn_B.py
@@ -1,2 +1,12 @@
-# CP3-suphakarn-buatip
-ตัวอย่าง ropository คอร์สเรียน Complete Python3 Programing โดย BorntoDev Co., Ltd.
+# เก็บคะแนนของแต่ละรายวิชา (เป็นทศนิยม)
+foundation_english = float(input("Enter your Foundation English score: "))
+general_business = float(input("Enter your General Business score: "))
+intro_computer = float(input("Enter your Introduction to Computer Systems score: "))
+programming = float(input("Enter your Computer Programming score: "))
+
+# แสดงผลคะแนน
+print("--- Your Score ---")
+print("Foundation English : ", foundation_english)
+print("General Business : ", general_business)
+print("Introduction to Computer Systems : ", intro_computer)
+print("Computer Programming : ", programming)
